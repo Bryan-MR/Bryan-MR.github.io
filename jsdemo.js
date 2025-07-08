0e6f073869a22f18e647887e486a794296506f7b
Bryan-MR <accalt3213@gmail.com>
2025-07-08 16:28:09 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,7 @@ console.log("This output is from the external JS file");
 console.log("Name: Bryan");
 console.log("School: CPCC");
 console.log("Grade: Freshman");
+
+var name = "Bryan";
+let school = "CPCC";
+var grade = "Freshman";
