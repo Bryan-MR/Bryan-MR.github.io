52af1ceff538f4306d0f7eeef2be478c7aef76c4
Bryan-MR <accalt3213@gmail.com>
2025-07-10 16:52:57 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -2,3 +2,18 @@ var course = "Technology";
 let myName = "Bryan";
 
 console.log(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+aler(`This page was created in the STEM PREP ${course} by ${myName}`):
+
+function output()
+{
+  let name = document.getElementById("name").value;
+  var school = document.getElementById("school").value;
+  let grade = document.getElementById("grade").value;
+
+  let output = "Name: <b>" + name + "</b><br>";
+  output += "Grade: <b>" + grade + "</b><br>";
+  output += "School: <b>" + school + "</b><br>";
+
+  document.getElementById("output").innerHTML = output;
+}
