1bdb6bb12d62cc36444d6997fde7b466ab73b161
Bryan-MR <accalt3213@gmail.com>
2025-07-17 16:47:11 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -10,3 +10,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let circleMarker = L.circle(coordinates, {
+  color: "cyan",
+  fillcolor: "white",
+  fillopacity: 0.25,
+  radius: 50
+}).bindPopup(`<iframe width="280" height="260" src="https://www.youtube.com/embed/eE65MVsnI4Y?si=drc0mSiupgsDeYr5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
